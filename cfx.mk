70bd32da549a73ae14bfe7f55db69d3b52c5da0c
RaymanFX <raymanfx@gmail.com>
2013-07-03 22:28:46 +0200

    cfx: do not override device props here

--- cfx.mk
+++ cfx.mk
@@ -14,6 +14,3 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, vendor/cfx/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT28h_1267-9107 BUILD_FINGERPRINT=SEMC/LT28h_1267-9107/LT28h:4.0.4/6.1.E.3.7/-PN-zw:user/release-keys PRIVATE_BUILD_DESC="LT28h-user 4.0.4 6.1.E.3.7 -PN-zw test-keys"
-
-PRODUCT_NAME := cfx_aoba
-PRODUCT_DEVICE := aoba
