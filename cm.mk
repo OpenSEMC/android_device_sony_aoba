eb40aa467ee3856472293b7a151a452fc59f587d
MrGezz <u_gezz@yahoo.com>
2013-12-21 00:16:56 +0800

    fix up allign

--- cm.mk
+++ cm.mk
@@ -31,7 +31,10 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT28h_1267-9107 BUILD_FINGERPRINT=SEMC/LT28h_1267-9107/LT28h:4.0.4/6.1.E.3.7/-PN-zw:user/release-keys PRIVATE_BUILD_DESC="LT28h-user 4.0.4 6.1.E.3.7 -PN-zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=LT28h \
+BUILD_FINGERPRINT="SEMC/LT28h_1267-9107/LT28h:4.0.4/6.1.E.3.7/-PN-zw:user/release-keys" \
+PRIVATE_BUILD_DESC="LT28h-user 4.0.4 6.1.E.3.7 -PN-zw test-keys"
 
 PRODUCT_NAME := cm_aoba
 PRODUCT_DEVICE := aoba
