13c71c8d6834f45c23a8024f4c6e06e2de3482b8
MrGezz <u_gezz@yahoo.com>
2013-12-21 03:24:49 +0800

    revert some change

--- full_aoba.mk
+++ full_aoba.mk
@@ -29,10 +29,30 @@ PRODUCT_PACKAGES := \
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+ifneq ($(TARGET_BUILD_VARIANT),codefirex)
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+else
+
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cfx/config/gsm.mk)
+
+# Boot animation
+PRODUCT_COPY_FILES += \
+    vendor/cfx/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
+
+# Inherit some common CFX stuff.
+$(call inherit-product, vendor/cfx/config/common_full_phone.mk)
+endif
+
 # Inherit from aoba device
 $(call inherit-product, device/sony/aoba/aoba.mk)
 
+# Aoba vendor blobs
+$(call inherit-product-if-exists, vendor/sony/aoba/aoba-vendor.mk)
 
+# Enhanced NFC - not yet present in CFX
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_aoba
 PRODUCT_DEVICE := aoba
