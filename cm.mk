67e0de7cfab39ed47824cfa07ed45d027ad25f3d
MrGezz <u_gezz@yahoo.com>
2013-12-20 16:41:34 +0800

    aoba: call cm full_phone vendor config

--- cm.mk
+++ cm.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2012 FXP (FreeXperia)
+# Copyright (C) 2013 The Open SEMC Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 $(call inherit-product, device/sony/aoba/full_aoba.mk)
 
 # Inherit CM common GSM stuff.
@@ -5,6 +20,9 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
