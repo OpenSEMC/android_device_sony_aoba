69b2dbafaddaa982d5a78747187d914da0bd2e72
MrGezz <u_gezz@yahoo.com>
2014-01-15 08:45:10 +0800

    remove ril properties

--- aoba.mk
+++ aoba.mk
@@ -66,19 +66,6 @@ PRODUCT_COPY_FILES += \
  
 $(call inherit-product-if-exists, vendor/sony/aoba/aoba-vendor.mk)
 
-# Radio and Telephony
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.transmitpower=true \
-    persist.radio.add_power_save=1
-
-# lib ril
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libril-qc-qmi-1.so
-
-# Do not power down SIM card when modem is sent to Low Power Mode.
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.apm_sim_not_pwdn=1
-
 # Wifi
 BOARD_WLAN_DEVICE_REV := bcm4330_b2
 WIFI_BAND             := 802_11_ABG
